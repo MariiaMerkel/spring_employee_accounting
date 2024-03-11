2679bbc1321296715ed89f8a337458c92d969c1a
Mariia <mariia_merkel@mail.ru>
2024-03-11 13:54:25 +0300

    initional commit

---
+++ src/main/java/ru/merkel/springemployeeaccounting/SpringEmployeeAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.merkel.springemployeeaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/merkel/springemployeeaccounting/SpringEmployeeAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkel.springemployeeaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
